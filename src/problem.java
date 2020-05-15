7233748890ba19ff71c995c3e2dc0119d9b87ff9
Miguel Damaceno <miguel.araujo669@gmail.com>
2020-05-15 19:16:52 -0300

    Cabecalho

--- src/problem.java
+++ src/problem.java
@@ -7,6 +7,10 @@ public class problem {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
 		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("--------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		
 		
 		sc.close();
 	}
