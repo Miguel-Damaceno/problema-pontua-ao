7e60f61cfdefcf4f28f131c79e13c977456f601d
Miguel Damaceno <miguel.araujo669@gmail.com>
2020-05-15 19:24:31 -0300

    Leitura do volume

--- src/problem.java
+++ src/problem.java
@@ -7,10 +7,19 @@ public class problem {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
 		
+		double qtdcompras, ticketmedio;
+		
+		// Cabeçalho //
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		System.out.println("");
 		
+		// Leitura do volume //
+		System.out.printf("Quantas compras o cliente fez no último ano?");
+		qtdcompras = sc.nextDouble();
+		System.out.printf("Qual o ticket médio?");
+		ticketmedio = sc.nextDouble();
 		
 		sc.close();
 	}
