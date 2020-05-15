aa934ab615de4a206fa0fda3a04e66cd677a5ae9
Miguel Damaceno <miguel.araujo669@gmail.com>
2020-05-15 20:28:13 -0300

    Scores de inadimplencia e pagamento

--- src/problem.java
+++ src/problem.java
@@ -7,7 +7,7 @@ public class problem {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
 		
-		double qtdcompras, ticketmedio, atrasos, ptscompras=0;
+		double qtdcompras, ticketmedio, atrasos, ptscompras=0, ptsinad=0, ptspag=0;
 		char modopagamento;
 		
 		// Cabeçalho //
@@ -32,29 +32,47 @@ public class problem {
 		//Score de volume de compras//
 		if (qtdcompras == 0) {
 			ptscompras = 0;
-			System.out.println("");
-			System.out.printf("Score de volume de compras = " + ptscompras);
 		}
 		else if (ticketmedio <= 3000 && qtdcompras <= 2) {
 			ptscompras = 20;
-			System.out.println("");
-			System.out.printf("Score de volume de compras = " + ptscompras);
 		}
 		else if (ticketmedio <= 3000 && qtdcompras > 2) {
 			ptscompras = 40;
-			System.out.println("");
-			System.out.printf("Score de volume de compras = " + ptscompras);
 		}
 		else if (ticketmedio > 3000) {
 			ptscompras = 60;
-			System.out.println("");
-			System.out.printf("Score de volume de compras = " + ptscompras);
 		}
 		else {
 			ptscompras = 0;
 		}
 		
+		System.out.println("");
+		System.out.println("Score de volume de compras = " + ptscompras + " pontos");
 		
+		//Scores de inadimplencia e pagamento//
+		//inadimplencia//
+		System.out.println("");
+		
+		if (atrasos > 1 || qtdcompras == 0) {
+			ptsinad = 0;
+		}
+		else if (qtdcompras > 0 && atrasos == 1) {
+			ptsinad = 15;
+		}
+		if (qtdcompras > 0 && atrasos == 0) {
+			ptsinad = 30;
+		}
+		
+		System.out.println("Score de inadimplência = " + ptsinad + " pontos");
+		
+		//forma de pagamento//
+		if (qtdcompras > 0 && modopagamento == 'd' || modopagamento == 'D') {
+			ptspag = 5;
+		}
+		if (qtdcompras > 0 && modopagamento == 'c' || modopagamento == 'C' || modopagamento == 'b' || modopagamento == 'B') {
+			ptspag = 10;
+		}
+		System.out.println("Score de forma de pagamento = " + ptspag + " pontos");
 		
 		sc.close();
 	}
